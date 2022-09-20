5ed971c6e18ca6bc109b5ba56ccd8f040993894c
Dorian VOYDIE <dorian.voydie@gmail.com>
2022-09-20 15:33:52 +0200

    Update

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ class Detection:
         print(model_name)
         if model_name:
             model = torch.hub.load('./yolov5', 'custom', path=model_name, force_reload=True, trust_repo=True, source='local')
-            model.conf=0.15
+            model.conf=0.05
             model.iou=0.45
             print("Model Loaded :!", model_name)
         else:
@@ -175,8 +175,8 @@ class Detection:
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
-        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
-        return labels, cord
+        labels, cord, conf = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-2], results.xyxyn[0][:, -2]
+        return labels, cord, conf
 
     def class_to_label(self, x):
         """
@@ -186,23 +186,24 @@ class Detection:
         """
         return self.classes[int(x)]
 
-    def plot_boxes(self, results, frame):
+    def plot_boxes(self, results, frame, conf_thres):
         """
         Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
         :param results: contains labels and coordinates predicted by model on the given frame.
         :param frame: Frame which has been scored.
         :return: Frame with bounding boxes and labels ploted on it.
         """
-        labels, cord = results
+        labels, cord, conf = results
         n = len(labels)
         x_shape, y_shape = frame.shape[1], frame.shape[0]
         for i in range(n):
             row = cord[i]
-            if row[4] >= 0.3:
+            conf_i = conf[i]
+            if conf_i >= conf_thres:
                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
                 bgr = (0, 255, 0)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]) + " " + str(round(float(str(conf_i)[8:-2]),2)), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
 
         return frame
 
@@ -237,7 +238,7 @@ def video_feed():
                 
                 start_time = time.time()
                 results = detector.score_frame(frame)
-                frame = detector.plot_boxes(results, frame)
+                frame = detector.plot_boxes(results, frame, conf_thres=detector.model.conf)
                 
                 end_time = time.time()
                 fps = 1/np.round(end_time - start_time, 2)
@@ -250,7 +251,6 @@ def video_feed():
                 
                 yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-                time.sleep(0.01)
     if request.method == 'GET':
         print(request.args)
         source = request.args['source']
